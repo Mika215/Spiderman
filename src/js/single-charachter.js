a4ed1144cde23bd95729f7af0b61406493353a8a
Michael <michaeltesfayg@gmail.com>
2021-10-23 00:22:26 +0200

    character fetch functions

--- src/js/single-charachter.js
+++ src/js/single-charachter.js
@@ -1,13 +1,66 @@
+const updateBtn = document.getElementById("update");
+const deleteBtn = document.getElementById("delete");
 
+updateBtn.addEventListener("click", () => {
+  console.log(
+    "I am the UPDATE button do you whant me to help you update your charachter"
+  );
+});
 
-const updateBtn=document.getElementById('update');
-const deleteBtn=document.getElementById('delete');
+deleteBtn.addEventListener("click", () => {
+  let action = confirm(`are you sure you want to delete this charachter`);
+  let didConfirm = true;
+  //the confirmation interaction is not complete revise it
+  if (!didConfirm) {
+    console.log("action aborted nothing deleted");
+  } else {
+    console.log("you have deleted this charachter");
+  }
+});
 
-updateBtn.addEventListener('click',()=>{
-    console.log("I am the UPDATE button do you whant me to help you update your charachter") 
-})
+/**
+ * Fetching data from the api
+ */
+const beCodeUrl = "https://character-database.becode.xyz/characters";
+const fetchCharacters = async () => {
+  try {
+    const res = await fetch(beCodeUrl);
+    if (res.status === 200) {
+      const characters = res.json();
 
+      return characters;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+/**
+ * may be i should rather create the HTML templet dynamically
+ * Rendering the fetched data to the user
+ */
+const imageDisplay = document.querySelector("single__image");
+const nameDisplay = document.querySelector("single__name");
+const shortDiscription = document.querySelector("single__short-discription");
+const detailedDiscription = document.querySelector(
+  "single__detailed-discription"
+);
+const nextBtn = document.getElementById("next");
+const base64Header = "data:image/jpeg;base64";
+let id;
+const renderCharacter = async () => {
+  const characters = await fetchCharacters();
+  for (let i = 0; i <= characters.length; i++);
+  const currentCharacter = characters[i];
+  let fetchedSrc = currentCharacter.image;
+  let base64 = `${base64Header},${fetchedSrc}`;
+  detailedDiscription.textContent = currentCharacter.description;
+  shortDiscription.textContent = currentCharacter.shortDescription;
+  nameDisplay.textContent = currentCharacter.name;
+  imageDisplay.src = base64;
+  id = currentCharacter.id;
+};
 
-deleteBtn.addEventListener('click',()=>{
-    console.log("I am the delete button are you sure you want to delete this charachter?") 
-})
+nextBtn.addEventListener("click", () => {
+  //This will be replaced with the "render character function"
+  console.log("next character rendering");
+});
