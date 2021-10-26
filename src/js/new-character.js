4a81f8b2f5c3642d2dde02a916a139c2dcd54a00
Frédéric Van Overmeire <frederic.vanovermeire@gmail.com>
2021-10-26 10:59:18 +0200

    add newchar fixe #5

---
+++ src/js/new-character.js
@@ -0,0 +1,20 @@
+
+ document.querySelector("#submit").addEventListener("click", async() => {
+     const image = document.querySelector("#new-image")
+    const name = document.querySelector("#new-name").value;
+    const shortDescription = document.querySelector("#shortDescription").value;
+    const description = document.querySelector("#markdown");
+    if (name !== "" && shortDescription !== "" && description !== "") {
+        const result = await fetch("https://character-database.becode.xyz/characters", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: name,
+                shortDescription: shortDescription,
+                description: description
+            })
+        });
+    }
+})
